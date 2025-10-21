07c41376cb442778f38d0fcb0b4b42e63952f8ad
dejworj <dejworj@shure.com>
2025-10-21 16:14:57 -0500

    added agent fuction calling

--- ai-agent/functions/get_files_info.py
+++ ai-agent/functions/get_files_info.py
@@ -1,4 +1,6 @@
 import os
+from google.genai import types
+
 
 # ** Treat the directory as a relative path inside working_directory
 def get_files_info(working_directory, directory="."):
@@ -8,7 +10,7 @@ def get_files_info(working_directory, directory="."):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     if not os.path.isdir(target_dir):
         return f'Error: "{directory}" is not a directory'
-    
+
     # Build string representing contents of the directory
     file_string = ""
     try:
@@ -16,6 +18,21 @@ def get_files_info(working_directory, directory="."):
             child_path = os.path.join(target_dir, content)
             file_string = f"{file_string}- {content}: file_size={os.path.getsize(child_path)} bytes, is_dir={os.path.isdir(child_path)}\n"
     except Exception as e:
-        return (f"Error: {e}")
-    
-    return (file_string)
+        return f"Error: {e}"
+
+    return file_string
+
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
