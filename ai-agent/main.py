ec07ccd2c6fffe3f028cfbd2fc5b4321e2d82468
dejworj <dejworj@shure.com>
2025-10-22 16:30:37 -0500

    adding feedback loop

--- ai-agent/main.py
+++ ai-agent/main.py
@@ -45,6 +45,10 @@ def generate_content(client, messages, verbose):
             tools=[available_functions], system_instruction=system_prompt
         ),
     )
+
+    for candidate in response.candidates:
+        messages.append(candidate.content)
+
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
