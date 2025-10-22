549fb6ce24f569dd2e0bb469d8c42b44c3dd06d5
dejworj <dejworj@shure.com>
2025-10-22 16:06:00 -0500

    added the call_fuction

--- ai-agent/functions/call_function.py
+++ ai-agent/functions/call_function.py
@@ -1,9 +1,10 @@
 from google.genai import types
 
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.run_python_file import schema_run_python_file
-from functions.write_file import schema_write_file
+from functions.get_files_info import schema_get_files_info, get_files_info
+from functions.get_file_content import schema_get_file_content, get_file_content
+from functions.run_python_file import schema_run_python_file, run_python_file
+from functions.write_file import schema_write_file, write_file
+from .data import WORKING_DIR
 
 available_functions = types.Tool(
     function_declarations=[
@@ -13,3 +14,42 @@ available_functions = types.Tool(
         schema_write_file,
     ]
 )
+
+
+def call_function(function_call_part, verbose=False):
+    func_name = function_call_part.name
+    func_args = dict(function_call_part.args)
+    function_map = {
+        "get_files_info": get_files_info,
+        "get_file_content": get_file_content,
+        "run_python_file": run_python_file,
+        "write_file": write_file,
+    }
+
+    if verbose:
+        print(f"Calling function: {func_name}({func_args})")
+    else:
+        print(f" - Calling function: {func_name}")
+
+    if func_name not in function_map:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=func_name,
+                    response={"error": f"Unknown function: {func_name}"},
+                )
+            ],
+        )
+
+    func_args["working_directory"] = WORKING_DIR
+    function_result = function_map[func_name](**func_args)
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=func_name,
+                response={"result": function_result},
+            )
+        ],
+    )
--- ai-agent/main.py
+++ ai-agent/main.py
@@ -3,7 +3,7 @@ import os
 from google import genai
 from google.genai import types
 from dotenv import load_dotenv
-from functions.call_function import available_functions
+from functions.call_function import available_functions, call_function
 from prompts import system_prompt
 
 
@@ -48,14 +48,24 @@ def generate_content(client, messages, verbose):
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
-    if response.function_calls:
-        for function_call_part in response.function_calls:
-            print(
-                f"Calling function: {function_call_part.name}({function_call_part.args})"
-            )
-    else:
-        print("Response:")
-        print(response.text)
+
+    if not response.function_calls:
+        return response.text
+
+    function_responses = []
+    for function_call_part in response.function_calls:
+        function_call_result = call_function(function_call_part, verbose)
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("empty function call result")
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        function_responses.append(function_call_result.parts[0])
+
+        if not function_responses:
+            raise Exception("no function responses generate, exiting")
 
 
 if __name__ == "__main__":
